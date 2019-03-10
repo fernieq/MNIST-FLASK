4e09c93fa71ecff742e41e3c34d362725fba668d
Fernie Qin <yiq028@ucsd.edu>
2019-03-10 04:20:45 -0700

    changed the port setting

--- app.py
+++ app.py
@@ -109,6 +109,5 @@ def formatImage(imgData):
 #127.0.0.1:5000
 if __name__ == '__main__':
     app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
 
